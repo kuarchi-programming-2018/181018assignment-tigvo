2f6bb526616451c2cd0583fe37b73563a62a1157
tigvo <43875078+tigvo@users.noreply.github.com>
2018-10-18 23:50:40 +0900

    課題提出
    一部しか間に合いませんでした、すいません。

---
+++ Paiza/5-04-3.py
@@ -0,0 +1,8 @@
+points={"国語":70,"算数":35,"英語":52}
+sum=0
+
+for i in points:
+    sum+=points[i]
+    
+print(int(sum))
+
---
+++ Paiza/6-06-2.py
@@ -0,0 +1,2 @@
+seven_Array=[[7 for i in range(4)]for j in range(5)]
+print(seven_Array)
